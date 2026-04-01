158f897fab21ec7b2d9285bd4fd4954795f35a72
myplc <plctohmi@gmail.com>
2026-04-01 13:53:53 +0900

    정리후 새시작

---
+++ nodejs/n01.js
@@ -0,0 +1,27 @@
+const { createServer } = require("http");
+const port = 4000;
+const site = "네이버다!";
+const server = createServer((req, res) => {
+  res.end(`
+    <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>하이퍼링크</title>
+  </head>
+  <body>
+    <h1>하이퍼링크</h1>
+    <hr />
+    <a href="index.html">홈으로</a><br /><br />
+    <a href="https://naver.com">${site}</a>
+    <br />
+    <a href="https://daum.net">다음</a>
+  </body>
+</html>
+`);
+});
+
+server.listen(port, () => {
+  console.log("서버가 동작하였습니다.");
+});
